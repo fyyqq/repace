29772ad8d85b83646e3f6a824df6cfc6bf20dc68
unknown <afiqakimy123@gmail.com>
2023-07-23 22:03:48 +0800

    make functional navbar toggler with javascript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const toggler = document.querySelector('.navbar-toggler');
+
+toggler.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (!toggler.className.includes('collapsed')) {
+        toggler.style.transform = 'rotate(90deg)';
+        toggler.style.transition = 'transform .3s ease';
+    } else {
+        toggler.style.transform = 'rotate(0deg)';
+    }
+});
